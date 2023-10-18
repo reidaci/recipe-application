eaeba2b5b2411cb9faafdadaf0f8cc8e88d04eff
Rei Daci <reiz.daci@outlook.com>
2023-10-18 23:10:25 +0200

    Fix Recipes (Services )

--- src/app/shared/data-storage.service.ts
+++ src/app/shared/data-storage.service.ts
@@ -2,6 +2,7 @@ import { RecipeService } from './../recipes/recipe.service';
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Recipe } from '../recipes/recipe.model';
+import { map, tap } from 'rxjs/operators';
 
 @Injectable({ providedIn: 'root' })
 export class DataStorageService {
@@ -20,12 +21,23 @@ export class DataStorageService {
   }
 
   fetchRecipes() {
-    this.http
+    return this.http
       .get<Recipe[]>(
         'https://recipes-application-4c80f-default-rtdb.firebaseio.com/recipes.json'
       )
-      .subscribe((recipes) => {
-        this.RecipeService.setRecipes(recipes);
-      });
+      .pipe(
+        map((recipes) => {
+          return recipes.map((recipe) => {
+            return {
+              ...recipe,
+              ingredients: recipe.ingredients ? recipe.ingredients : [],
+            };
+          });
+        }),
+        tap(recipes => {
+          this.RecipeService.setRecipes(recipes);
+        })
+      )
+
   }
 }
