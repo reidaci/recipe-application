5bb4b93600d2decc6bc35cd449553c822c95cb4d
Rei Daci <reiz.daci@outlook.com>
2023-10-23 12:14:55 +0200

    Alert Modal Component

---
+++ src/app/shared/alert/alert.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { Input } from '@angular/core';
+
+@Component({
+  selector: 'app-alert',
+  templateUrl: 'alert.component.html',
+  styleUrls: ['alert.component.css'],
+})
+export class AlertComponent {
+  @Input() message: string;
+}
